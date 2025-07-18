effece2475ea9f029fb9a10447b8eeb1537875ee
chunyin7 <miles.c.y.ng@gmail.com>
2025-07-18 23:47:06 +1000

    embed tokens

--- include/embedding/embedding.h
+++ include/embedding/embedding.h
@@ -3,3 +3,6 @@
 
 // emit randomized embedding matrix
 Array *init_embedding(size_t dim, size_t voc_size);
+
+// embed token ids into embedding matrix
+Array *embed(Array *ids, Array *emb_mat, size_t dim);
--- src/embedding/embedding.c
+++ src/embedding/embedding.c
@@ -4,12 +4,36 @@
 #include <time.h>
 
 Array *init_embedding(size_t dim, size_t voc_size) {
-  Array *emb = arr_init(sizeof(float));
+  Array *emb = arr_init(sizeof(Array *));
   srand(time(NULL));
 
   for (size_t i = 0; i < voc_size; i++) {
-    float val = (float)rand() / (float)RAND_MAX;
-    arr_append(emb, &val);
+    Array *tok = arr_init(sizeof(float));
+    for (size_t j = 0; j < dim; j++) {
+      float val = (float)rand() / (float)RAND_MAX;
+      arr_append(tok, &val);
+    }
+
+    arr_append(emb, &tok);
+  }
+
+  return emb;
+}
+
+Array *embed(Array *ids, Array *emb_mat, size_t dim) {
+  Array *emb = arr_init(sizeof(Array *));
+
+  for (size_t i = 0; i < ids->len; i++) {
+    Array *tok_emb = arr_init(sizeof(float));
+    long id = ((long *)ids->data)[i];
+    Array *emb_mat_row = &((Array *)emb_mat->data)[id];
+
+    for (size_t j = 0; j < dim; j++) {
+      float val = ((float *)emb_mat_row->data)[j];
+      arr_append(tok_emb, &val);
+    }
+
+    arr_append(emb, &tok_emb);
   }
 
   return emb;
