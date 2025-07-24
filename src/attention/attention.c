672a4dc8b19b8bb60fdf890f9d385ab6b7456c26
chunyin7 <miles.c.y.ng@gmail.com>
2025-07-24 17:08:18 +1000

    alter weight normalization

--- src/attention/attention.c
+++ src/attention/attention.c
@@ -14,23 +14,39 @@ Matrix *self_attention(Matrix *token_embedding_matrix, Matrix *Wq, Matrix *Wk, M
   // scores = Q * K^T
   Matrix *scores = matrix_multiply(Q, K_T);
 
+  // normalize by dimension of key/query vectors (row-wise in K)
+  // to scale gradients
+  size_t d_k = K->cols;
+
+  for (size_t i = 0; i < scores->rows; i++) {
+    for (size_t j = 0; j < scores->cols; j++) {
+      matrix_set(scores, i, j, matrix_get(scores, i, j) / sqrt((double)d_k));
+    }
+  }
+
   // normalize with softmax
   double sigma = 0;
   for (size_t i = 0; i < scores->rows; i++) {
     for (size_t j = 0; j < scores->cols; j++) {
-      sigma += exp(((double *)scores->data)[i * scores->cols + j]);
+      sigma += exp(matrix_get(scores, i, j));
     }
   }
 
   for (size_t i = 0; i < scores->rows; i++) {
     for (size_t j = 0; j < scores->cols; j++) {
-      double normalized = matrix_get(scores, i, j) / sigma;
+      double normalized = exp(matrix_get(scores, i, j)) / sigma;
       matrix_set(scores, i, j, normalized);
     }
   }
 
   // weight with V for context matrix
   Matrix *context = matrix_multiply(scores, V);
+
+  matrix_free(scores);
+  matrix_free(K_T);
+  matrix_free(Q);
+  matrix_free(K);
+  matrix_free(V);
 
   return context;
 }
